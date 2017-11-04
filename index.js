e7cabd7f423707425de87339fd53b2811c55f4e1
stephan lalka <slalka@gmail.com>
2017-11-04 23:47:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(o,k,v) {
+  return Object.assign(o, {[k]:v})
+
+}
+function destructivelyUpdateObjectWithKeyAndValue(o,k,v) {
+  
+}
+
+function deleteFromObjectByKey(o,k){
+
+}
+function destructivelyDeleteFromObjectByKey(o,k){
+
+}
